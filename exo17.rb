a6f5b6c1c74bc4bc0d63e7f9e3544d71548e8b4b
houdheyfakaddouri <houdheyfakaddouri08@gmail.com>
2025-01-15 08:23:32 +0100

    Add files via upload

--- exo17.rb
+++ exo17.rb
@@ -1,11 +1,14 @@
-def print_pyramid(height)
-    (1..height).each do |i|
-        spaces = '#' *(height - 1)
-        stars =' *'  *(2*1-1)
-        puts spaces +stars
-    end
+puts "Donne moi un nombre entre 1 et 25"
+number = gets.chomp.to_i
+
+while number < 1 || number > 25
+  puts "Le nombre n'est pas entre 1 et 25"
+  print "> "
+  number = gets.chomp.to_i
 end
 
-puts " combien veut d etage"
-height = gets.chomp.to_i
-print_pyramid(height)
+1.upto(number) do |etages|
+  espaces = number - etages
+  dieses = 2 * etages - 1
+  puts " " * espaces + "#" * dieses
+end 
